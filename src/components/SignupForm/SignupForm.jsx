47fc45900e0811005e796770a2182b1a432b9d1d
jason <jason_tan75@hotmail.com>
2024-07-24 13:36:45 -0400

    late add-commit-push: react-router-dom, components, services, cors in backend

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,104 @@
+// SignupForm.jsx
+
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import * as authService from '../../services/authService';
+
+const initialState = {
+    username: '',
+    password: '',
+    passwordConf: '',
+};
+
+const SignupForm = (props) => {
+    const navigate = useNavigate();
+    const [message, setMessage] = useState(['']);
+    const [formData, setFormData] = useState(initialState);
+
+    const updateMessage = (msg) => {
+        setMessage(msg);
+    };
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     updateMessage('');
+    //     console.log(formData); // this line will print the form data to the console
+    //     setFormData(initialState);
+    // };
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault()
+    //     try {
+    //         props.setUser(formData) // this will modify the state in the App component
+    //         navigate('/') // upon redirect you will see the "Dashboard" page
+    //     } catch (err) {
+    //         updateMessage(err.message);
+    //     }
+    // };
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const newUserResponse = await authService.signup(formData)
+            props.setUser(newUserResponse.user);
+            navigate('/')
+        } catch (err) {
+            updateMessage(err.message)
+        }
+    }
+
+    const { username, password, passwordConf } = formData;
+
+    const isFormInvalid = () => {
+        return !(username && password && password === passwordConf);
+    };
+
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={username}
+                        name="username"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        name="password"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirm">Confirm Password:</label>
+                    <input
+                        type="password"
+                        id="confirm"
+                        value={passwordConf}
+                        name="passwordConf"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign Up</button>
+                    <Link to="/">
+                        <button>Cancel</button>
+                    </Link>
+                </div>
+            </form>
+        </main>
+    );
+};
+
+export default SignupForm;
